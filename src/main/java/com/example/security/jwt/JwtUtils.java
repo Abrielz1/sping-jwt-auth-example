e5e4bb65fe3ccf89e17c74c6c231464a630d0834
Abriel <abrielz11@gmail.com>
2024-02-05 19:52:18 +0300

    coding goes on

--- src/main/java/com/example/security/jwt/JwtUtils.java
+++ src/main/java/com/example/security/jwt/JwtUtils.java
@@ -1,8 +1,14 @@
 package com.example.security.jwt;
 
+import com.example.security.AppUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -12,5 +18,19 @@ public class JwtUtils {
     private String someSecretKey;
 
     @Value("${app.jwt.tokenExpiration}")
-    private String tokenExpiration;
+    private Duration tokenExpiration;
+
+    public String generateJwtToken(AppUserDetails userDetails) {
+        return generateTokenFromUserName(userDetails.getUsername());
+    }
+
+    private String generateTokenFromUserName(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + tokenExpiration.toMillis()))
+                .signWith(SignatureAlgorithm.ES512, someSecretKey)
+                .compact();
+
+    }
 }
